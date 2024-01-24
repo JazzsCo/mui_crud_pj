813151359f9285159533344fe3c673171173b74c
Jzz-C <ygyi8632@gmail.com>
2024-01-24 13:24:26 +0630

    Set up with prisma and mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Input from "@/components/input";
 
 export default function Home() {
   return (
@@ -27,6 +28,10 @@ export default function Home() {
           </a>
         </div>
       </div>
+
+      <Input />
+
+      <div>Hello</div>
 
       <div className={styles.center}>
         <Image
